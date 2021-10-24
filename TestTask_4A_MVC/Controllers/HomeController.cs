fdb1da83f7301ba8a72ed909dd247867f0ef8855
Dmitrii Rybalko <rybalko02@gmail.com>
2021-10-24 14:49:15 +0300

    Added bootstrap and requests support

--- TestTask_4A_MVC/Controllers/HomeController.cs
+++ TestTask_4A_MVC/Controllers/HomeController.cs
@@ -20,5 +20,21 @@ namespace TestTask_4A_MVC.Controllers
         {
             return View(db.Books.ToList());
         }
+
+        [HttpGet]
+        public IActionResult Read(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Read(Request request)
+        {
+            db.Requests.Add(request);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + request.User + ", за заявку!";
+        }
     }
 }
