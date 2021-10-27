378d1c9fc197a2cdfc3bde41f2d4288a3fa4318a
Dmitrii Rybalko <rybalko02@gmail.com>
2021-10-27 14:41:59 +0300

    Implemented "Add book" feature

--- TestTask_4A_MVC/Controllers/HomeController.cs
+++ TestTask_4A_MVC/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using TestTask_4A_MVC.Models;
 
@@ -35,6 +36,21 @@ namespace TestTask_4A_MVC.Controllers
             // сохраняем в бд все изменения
             db.SaveChanges();
             return "Спасибо, " + request.User + ", за заявку!";
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Add(Book book)
+        {
+            book.DatePublished = DateTime.Now;
+            db.Books.Add(book);
+            db.SaveChanges();
+            return "Спасибо за добавление!";
         }
     }
 }
